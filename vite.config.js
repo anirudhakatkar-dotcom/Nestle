385ca42742f277bf1d5126f8b86aa2473fd5e9ff
anirudhakatkar-dotcom <anirudhakatkar@gmail.com>
2026-05-22 09:34:28 +0000

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
-
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? `/${repo}/` : '/',
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Nestle',
-        short_name: 'Nestle',
-        description: 'Your home, organised',
-        theme_color: '#0F0F0F',
-        background_color: '#0F0F0F',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '.',
-        icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
-        ],
-      },
-    }),
-  ],
-})
+  base: '/Nestle/',
+    plugins: [
+        react(),
+            VitePWA({
+                  registerType: 'autoUpdate',
+                        manifest: {
+                                name: 'Nestle',
+                                        short_name: 'Nestle',
+                                                description: 'Your home, organised',
+                                                        theme_color: '#0F0F0F',
+                                                                background_color: '#0F0F0F',
+                                                                        display: 'standalone',
+                                                                                orientation: 'portrait',
+                                                                                        start_url: '.',
+                                                                                                icons: [
+                                                                                                          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+                                                                                                                    { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
+                                                                                                                            ],
+                                                                                                                                  },
+                                                                                                                                      }),
+                                                                                                                                        ],
+                                                                                                                                        })
